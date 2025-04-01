7c3f63c4ce1b7b9ceaa0f838d68182c0ba9caaaf
M.Ifsal Fadhilah <mifsalf05@gmail.com>
2025-04-01 15:56:55 +0700

    Update Bengkel.cpp
    buat sesi login, nampilin list spare part dan servis.
    next: buat bagian nota, dan ngedit layanan

--- Bengkel.cpp
+++ Bengkel.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 /*jadi, buat jumlah sparepat, harga servis, memperbarui data(menambah,mengurangi stok), nota pembayaran, 
 opsional= jika servis penuh ada opsi delay diservis */
+bool bagLogin=true;
 
 struct usnpasslogin{
     string id= "k123";
@@ -14,75 +16,135 @@ struct sparepat{
     string nama;
     int jumlah;
     int harga;
-};sparepat sp[100]={{"busi",10,20000},{"oli",20,50000},{"kampas rem",15,30000},{"lampu",5,10000}};
+};sparepat sp[]={{"busi",10,20000},{"oli",20,50000},{"kampas rem",15,30000},{"lampu",5,10000},{"spion(sepasang)",8,40000}};
 
 struct servis{
     string nama;
     int harga;
-};servis sr[100]={{"ganti oli",20000},{"ganti busi",15000},{"ganti kampas rem",25000},{"ganti lampu",10000}};
+};servis sr[]={{"ganti oli",70000},{"ganti busi",50000},{"ganti kampas rem",40000},{"ganti lampu",20000}};
 
 //bagian login
-void sesilogin(int x,bool bagLogin){
-    if(x>=0){
-        cout<<"Masukkan Id: ";getline(cin>>ws,login.id);
-        cout<<"Masukkan Password: ";getline(cin>>ws,login.pass);
-        if(login.id!="k123"||login.pass!="123"){
-            cout<<"id atau password anda salah\n";
-            cout<<"anda memiliki "<<x<<" kesempatan lagi\n\n";
-            sesilogin(x-1,bagLogin);
+void sesiLogin(int x){
+    system("cls");
+    if(x>0){
+        string usn,password;
+        cout<<"Username: ";cin>>usn;
+        cout<<"Password: ";cin>>password;
+        if(usn!=login.id||password!=login.pass){
+            x--;
+            cout<<"Username atau Password salah\n";
+            cout<<"Tersisa "<<x<<" kesempatan\n\n";
+            system("pause");
+            sesiLogin(x);
         }
         else{
-            cout<<"halo admin\n";
-            x=-2;
-            bagLogin=true;
-            
+            cout<<"Login berhasil\n";
+            return;
         }
     }
-    if(x=-1){
-        bagLogin=false;
+    else{
+        cout<<"Kesempatan habis\n";
+        exit(0);
     }
 }
 
-int main(){
-    system("cls");
-    //masih ngebug dibagian loginnya kesempatan habis malah masuk ke menu utama
-    int x=2;
-    bool bagLogin=true;
-    sesilogin(x,bagLogin);
-    cout<<x<<endl;
-    if (bagLogin==false){
-        cout<<"cihuy";
-        return 0;
+//nampilin list sparepart
+string header1="LIST SPAREPART";
+void funcSparepart(){
+    cout<<setfill('=')<<setw(44)<<""<<endl;
+    cout<<setfill(' ')<<"|"<<left<<setw(42)<<header1<<"|"<<endl;
+    cout<<setfill('-')<<"|"<<right<<setw(43)<<"|";
+    cout<<setfill(' ')<<"\n| No "<<left<<setw(22)<<"| Nama Barang"<<setw(9)<<"| Harga"<<"| Stok |"<<endl;
+    for(int i=0;i<sizeof(sp)/sizeof(sp[0]);i++){
+        cout<<setfill(' ')<<left<<"| "<<setw(3)<<i+1<<"| "<<setw(20)<<sp[i].nama<<"| "<<setw(7)<<sp[i].harga<<"| "<<setw(5)<<sp[i].jumlah<<"|"<<endl;
     }
-    else{
+    cout<<setfill('=')<<setw(44)<<""<<endl;
+}
+
+//nampilin list servis
+string header2="LIST SERVIS";
+void funcServis(){
+    cout<<setfill('=')<<setw(44)<<""<<endl;
+    cout<<setfill(' ')<<"|"<<left<<setw(42)<<header2<<"|"<<endl;
+    cout<<setfill('-')<<"|"<<right<<setw(43)<<"|";
+    cout<<setfill(' ')<<"\n| No "<<left<<setw(28)<<"| servis"<<setw(10)<<"| Harga"<<"|"<<endl;
+    cout<<setfill('-')<<"|"<<right<<setw(43)<<"|"<<endl;
+    for(int i=0;i<sizeof(sr)/sizeof(sr[0]);i++){
+        cout<<setfill(' ')<<left<<"| "<<setw(3)<<i+1<<"| "<<setw(26)<<sr[i].nama<<"| "<<setw(8)<<sr[i].harga<<"|"<<endl;
+    }
+    cout<<setfill('=')<<right<<setw(44)<<"";
+}
+
+//
+void listNota(){
+//nampilin apa aja yang udah dipesan abistuh nanya mau nambah pesanan atau nggak, kalau nggak langsung bayar
+}
+
+void edit(){
+
+}
+
+void layanan(){
+    int pilih;
+    cout<<"Layanan Produk/Servis\n";
+    cout<<"1. Sparepart\n";
+    cout<<"2. Servis\n";
+    cout<<"Pilih: ";cin>>pilih;
+    switch (pilih)
+    {
+    case 1:
+        funcSparepart();
+        break;
+    case 2:
+        funcServis();
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void buatPesanan(){
+    layanan();
+    cout<<"eak";
+}
+
+void mainMenu(){
     int pilih;
     cout<<"====LIST====\n";
-    cout<<"1. Sparepat\n";//istilahnya cuman jual barangnya aja 
-    cout<<"2. Servis\n";//lebih ke jual barangnya+jasanya(ganti oli/aki) pengecekan/servis,sama kalau ganti oli,bersihin cvt, dll
-    cout<<"3. pesanan\n";//lihat pesanan sekaligus, terus mau nambah/bayar/ubah pesanan
-    cout<<"4. edit \n";//buat ngedit sparepat/servis
+    cout<<"1. List Layanan\n";//istilahnya cuman jual barangnya aja 
+    cout<<"2. Membuat Pesanan\n";//lebih ke jual barangnya+jasanya(ganti oli/aki) pengecekan/servis,sama kalau ganti oli,bersihin cvt, dll
+    cout<<"3. List nota\n";//lihat pesanan sekaligus, terus mau nambah/bayar/ubah pesanan
+    cout<<"4. Edit Layanan \n";//buat ngedit sparepat/servis
     cout<<"5. Keluar \n";//proses selesai
     cout<<"============\n";
     cout<<"Pilih: ";cin>>pilih;
     switch (pilih)
     {
     case 1:
-        /* code */
+        layanan();
         break;
     case 2:
-        /* code */
+        buatPesanan();
         break;
     case 3:
-        /* code */
+        listNota();
         break;
     case 4:
-        /* code */
+        edit();
         break;
     case 5:
-        /* code */
+        exit(0);
         break;
     default:
         break;
     }
+
 }
+
+
+int main(){
+    system("cls");
+    sesiLogin(3);
+    mainMenu();
 }
